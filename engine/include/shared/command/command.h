c98684dc4343fbbe9d71436e65031df59d24f4b2
Mansky446 <mansky446@gmail.com>
2026-03-07 07:16:55 +0700

    Update command.h

--- engine/include/shared/command/command.h
+++ engine/include/shared/command/command.h
@@ -8,6 +8,7 @@
 
 #define COMMAND_ID_LISTS \
     _DISPATCHER(COMMAND_ID_INIT, "init", (sizeof("init") - 1)) \
+    _DISPATCHER(COMMAND_ID_UINIT, "uinit", (sizeof("uinit") - 1)) \
     _DISPATCHER(COMMAND_ID_CREATE, "create", (sizeof("create") - 1)) \
     _DISPATCHER(COMMAND_ID_EDIT, "edit", (sizeof("edit") - 1)) \
 \
@@ -22,6 +23,7 @@ typedef enum command_id {
 status_t command_process(matroot_t *matroot, string_t *error_message, const json_t *in, json_t *out);
 
 status_t command_init(matroot_t *matroot, string_t *error_message, json_t *out);
+status_t command_uinit(matroot_t *matroot, string_t *error_message, uint8_t palette_id, json_t *out);
 status_t command_create(matroot_t *matroot, string_t *error_message, uint8_t palette_id, model_type_t type, json_t *out);
 status_t command_edit(matroot_t *matroot, string_t *error_message, uint8_t palette_id, uint8_t model_id, uint8_t moption_id, uint8_t *variant_ids, uint8_t *option_ids, json_t *out);
 
