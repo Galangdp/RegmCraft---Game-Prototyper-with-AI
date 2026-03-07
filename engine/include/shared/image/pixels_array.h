5a26b444816357f3a9af9b827bd8f6166cf7a776
Mansky446 <mansky446@gmail.com>
2026-03-07 07:14:32 +0700

    Update pixels_array.h

--- engine/include/shared/image/pixels_array.h
+++ engine/include/shared/image/pixels_array.h
@@ -5,31 +5,13 @@
 #include "utils/memory.h"
 
 typedef struct pixels_array {
-  pixels_rgba_t *values;
-  size_t length;
-  size_t capacity;
+    pixels_rgba_t *values;
+    size_t length;
+    size_t capacity;
 } pixels_array_t;
 
-FORCE_INLINE void pixels_array_init(pixels_array_t *array) {
-  array->values = NULL;
-  array->length = 0;
-  array->capacity = 0;
-}
-
-FORCE_INLINE pixels_rgba_t *pixels_array_alloc(pixels_array_t *array) {
-  if (array->length >= array->capacity) {
-    array->capacity = array->capacity == 0 ? 8 : array->capacity * 2;
-    array->values =
-        memory_realloc(array->values, array->capacity * sizeof(pixels_rgba_t));
-  }
-  return array->values + array->length++;
-}
-
-FORCE_INLINE void pixels_array_deinit(pixels_array_t *array) {
-  memory_free(array->values);
-  array->values = NULL;
-  array->length = 0;
-  array->capacity = 0;
-}
+void pixels_array_init(pixels_array_t *array);
+pixels_rgba_t *pixels_array_alloc(pixels_array_t *array);
+void pixels_array_deinit(pixels_array_t *array);
 
 #endif /* IMAGE_PIXELS_ARRAY_H */
